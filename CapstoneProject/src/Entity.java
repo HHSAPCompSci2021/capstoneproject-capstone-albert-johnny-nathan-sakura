283f6e97c9debc58d21dd2e28060b055b30d4c62
Nathan_Gu2024 <nathangu2006@gmail.com>
2022-05-03 15:24:45 -0700

    entity shenanigans

--- CapstoneProject/src/Entity.java
+++ CapstoneProject/src/Entity.java
@@ -1,3 +1,4 @@
+import processing.core.PApplet;
 import processing.core.PImage;
 
 public class Entity {
@@ -8,11 +9,54 @@ public class Entity {
 	public int[] bounds; //{topLeftX, topLeftY, windowWidth, windowHeight}
 	
 	public Entity(double x, double y, double w, double h, boolean circle, PImage image) {
-		this.x = x; this.y = y; this.width = w; this.height = h; isCircleHitbox = circle; sprite = image;
+		this.x = x; 
+		this.y = y; 
+		this.width = w; 
+		this.height = h; 
+		isCircleHitbox = circle; 
+		sprite = image;
 	}
 	
 	public boolean isTouching(Entity e) {
+		//Rectangle-Rectangle
+		if(!e.getCircleHitbox())
+			if(isPointInside(e.getX(), e.getY()) || isPointInside(e.getX() + e.getWidth(), e.getY()) 
+			|| isPointInside(e.getX() + e.getWidth(), e.getY() + e.getHeight())
+			|| isPointInside(e.getX(), e.getY() + e.getHeight())) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		
+		//Circle-Circle
+		else if(e.getCircleHitbox() && this.getCircleHitbox()){			
+			double centerDist = Math.sqrt(Math.pow(getX() - e.getX(), 2) + Math.sqrt(Math.pow(getY() - e.getY(), 2)));
+			double radiusDist = getWidth()+ e.getWidth();
+			
+			if(centerDist <= radiusDist) {
+				return true;
+			}
+			else {
+				return false;
+			}
+
+		}
+		//Rectangle-Circle
+		else {
+			
+		}
 	}
+	
+	public boolean isPointInside(double x, double y) {
+		
+		if(x >= this.getX() && x <= this.getX() + this.getWidth() 
+		&& y >= this.getY() && y <= this.getY() + this.getHeight()) {
+			return true;
+		}
+		return false;
+	}
+
 	
 	public void act() {
 		
@@ -21,4 +65,27 @@ public class Entity {
 	public void draw(PApplet surface) {
 		
 	}
+	
+	public double getX() {
+		return x;
+	}
+	public double getY() {
+		return y;
+	}
+	public double getvx() {
+		return vx;
+	}
+	public double getvy() {
+		return vy;
+	}
+	public double getWidth() {
+		return width;
+	}
+	public double getHeight() {
+		return height;
+	}
+	public boolean getCircleHitbox() {
+		return isCircleHitbox;
+	}
+	
 }
