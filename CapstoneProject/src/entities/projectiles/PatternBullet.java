139cbedcd39e9180aa59f4b2176b96f1263670d5
johnny-zhang-420 <johnny.zhang4916@gmail.com>
2022-05-13 10:31:53 -0700

    constructor javadoc reeeeeee

--- CapstoneProject/src/entities/projectiles/PatternBullet.java
+++ CapstoneProject/src/entities/projectiles/PatternBullet.java
@@ -9,6 +9,13 @@ import java.util.ArrayList;
  */
 public class PatternBullet {
 	private ArrayList<Bullet> bullets;
+	/**
+	 * Creates a new instance of Bullet with the properties of Entity
+	 * @param x X-Coordinate
+	 * @param y Y-Coordinate
+	 * @param type: the type of bullet
+	 * @param dmg: the damage of the bullet
+	 */
 	public PatternBullet(int x, int y, int type, int dmg) {
 		bullets = new ArrayList<Bullet>();
 		if (type == 1) {
