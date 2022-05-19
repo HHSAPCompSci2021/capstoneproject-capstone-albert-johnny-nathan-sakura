5b5445ae6dd34e3659f0dffe130fadba7ec45def
Albert Yu <89153981+reku2@users.noreply.github.com>
2022-05-18 20:27:30 -0700

    Update Player.java

--- CapstoneProject/src/entities/mobs/Player.java
+++ CapstoneProject/src/entities/mobs/Player.java
@@ -35,7 +35,7 @@ public class Player extends Entity {
 		super(x,y,w,h,circle);
 		entityList = new ArrayList<Entity>();
 		this.playerNum = gameNum;
-		this.setHp(10000);
+		this.setHp(150);
 		multishot = false;
 		resetCD = 10;
 		movementMulti = 1;
